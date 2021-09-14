86fb0baf787fdc5f4cd7dea77fc3fdc7c738e7e1
kimsehwan96 <sktpghks138@gmail.com>
2021-09-15 01:26:33 +0900

    fxxx

--- 탐색/python/astart_search_n_queen.py
+++ 탐색/python/astart_search_n_queen.py
@@ -4,10 +4,7 @@ date : 2021.09.12
 """
 
 import queue
-from copy import deepcopy
 
-
-# 우선 goal 상태(목표)를 정하고 진행하기 위해서, 8-QUEEN 이라고 가정하고 시작한다.
 
 class State:
     def __init__(self, board_size: int, board: list, col: int = 0) -> None:
@@ -19,47 +16,51 @@ class State:
         new_board = self.board[:]  # list copy
         # 퀸을 두었을때, 해당 행을 퀸을 놓을 수 없는 위치로 표현
         pos = (row * self.board_size) + col
-        for i in range(self.board_size):
+        for i in range(col, self.board_size):
             line = row * self.board_size
             tmp = line + i
             if i == col:
                 new_board[tmp] = 2
             else:
-                if i > col:
-                    new_board[tmp] = 0
+                new_board[tmp] = 0
             # 퀸을 두었을 때, 해당 열에 퀸을 놓을 수 없는 위치로 표현
+        for i in range(self.board_size):
             line = (i * self.board_size) + col
-            if i >= col:
-                if i == row:
-                    new_board[line] = 2
-                else:
-                    new_board[line] = 0
-            # 오른쪽 방향 대각선, 퀸을 놓을 수 없는 위치로 처리
+            # if i >= col:
+            if i == row:
+                new_board[line] = 2
+            else:
+                new_board[line] = 0
+            # 대각선 퀸을 놓을 수 없는 위치로 처리
+        for i in range(col, self.board_size):
+            # if col == self.board_size:
+            #     break
             line = row * self.board_size + col
-            # UP SIDE
             calc = (line + i) - (self.board_size * i)
             if calc > 0:
-                # if calc != pos:
                 new_board[calc] = 0
             calc = (line + i) + (self.board_size * i)
-            if (self.board_size * self.board_size - 1) > calc > 0:
-                # if calc != pos:
+            if (self.board_size * self.board_size) > calc:
                 new_board[calc] = 0
         new_board[pos] = 2
 
         return State(self.board_size, new_board, col)
 
     def expand(self, col: int = 0) -> list:
+        """
+        자식노드를 확장하는 메소드
+        해당 열의 특정 row가 배치 가능한 row 일 경우만 자식 노드를 확장함
+        :param col: 열
+        :return: 자식 노드들(State)의 배열
+        """
         result = []
-        # 8개 각 행에 놓는 것을 연산자로 표현한다.
         if col < self.board_size:
             for i in range(self.board_size):
-                if self.board[i * self.board_size + col] == 1:
+                calc = i * self.board_size + col
+                if calc >= self.board_size * self.board_size:
+                    return []
+                if self.board[calc] == 1:
                     result.append(self.get_new_state(i, col))
-            # tmp = self.get_new_state(i, col)
-            # if tmp.board[(i * self.board_size) + col] != 0:
-            #     result.append(self.get_new_state(i, col))
-            # # result.append(tmp)
         return result
 
     def print_board(self) -> None:
@@ -74,7 +75,18 @@ class State:
         return self.h() + self.g()
 
     def h(self):
-        return sum([1 if self.board[i] == 0 else 0 for i in range(self.board_size * self.board_size)])  # board size
+        """
+        다음 col 이후에 존재하는 0의 개수를 평가한다.
+        :return:
+        """
+        # return sum([1 if self.board[i] == 0 else 0 for i in range(self.board_size * self.board_size)])  # board size
+        ret = 0
+        if self.col < self.board_size:
+            for i in range(self.col, self.board_size):
+                for i2 in range(self.board_size):
+                    if self.board[i2 * self.board_size + i] == 0:
+                        ret += 1
+        return ret
 
     def g(self):
         return self.col
@@ -89,31 +101,6 @@ class State:
                self.print_board() + "\n" + \
                "----------------"
 
-
-# init = [
-#     1, 1, 1, 1, 1, 1, 1, 1,
-#     1, 1, 1, 1, 1, 1, 1, 1,
-#     1, 1, 1, 1, 1, 1, 1, 1,
-#     1, 1, 1, 1, 1, 1, 1, 1,
-#     1, 1, 1, 1, 1, 1, 1, 1,
-#     1, 1, 1, 1, 1, 1, 1, 1,
-#     1, 1, 1, 1, 1, 1, 1, 1,
-#     1, 1, 1, 1, 1, 1, 1, 1,
-#     ]
-# 초기 상태 8x8 chess board / 1은 퀸을 둘 수 있는 위치라고 표현함.
-# 0은 퀸을 둘 수 없는 위치, 2는 퀸을 둔 위치
-
-# goal = [
-#     0, 0, 0, 2, 0, 0, 0, 0,
-#     0, 0, 0, 0, 0, 0, 2, 0,
-#     0, 0, 2, 0, 0, 0, 0, 0,
-#     0, 0, 0, 0, 0, 0, 0, 2,
-#     0, 2, 0, 0, 0, 0, 0, 0,
-#     0, 0, 0, 0, 2, 0, 0, 0,
-#     2, 0, 0, 0, 0, 0, 0, 0,
-#     0, 0, 0, 0, 0, 2, 0, 0
-#         ]
-# 목표 상태
 
 def check_is_goal(size: int, l: list) -> bool:
     for col in range(size):
@@ -139,7 +126,8 @@ if __name__ == '__main__':
     while not open_queue.empty():
         current = open_queue.get()
         print(current)
-        # if current.board.count(2) == N-1:
+        # if current.board.count(2) == N:
+        # col = current.col + 1
         if check_is_goal(N, current.board):
             print('탐색 성공')
             break
@@ -150,14 +138,13 @@ if __name__ == '__main__':
         else:
             print('탐색 실패')
         col = current.col + 1
-    # s = State(8, [1 for x in range(64)])
-    # t = s.get_new_state(0, 0)
-    # # print(t)
-    # tt = t.get_new_state(6,1)
-    # print(tt)
-    # # ns = t.expand(1)
-    # # for v in ns:
-    # #     print(v)
-    # # # print(t)
-    # # t2 = t.get_new_state(3,1)
-    # # print(t2)
+    # s = State(8, [1 for _ in range(8 * 8)])
+    # t = s.get_new_state(5, 0)
+    # t = t.get_new_state(3, 1)
+    # t = t.get_new_state(7, 2)
+    # t = t.get_new_state(1, 3)
+    # t = t.get_new_state(4, 4)
+    # t = t.get_new_state(2, 5)
+    # t = t.get_new_state(0, 6)
+    # t = t.get_new_state(6, 7)
+    # print(t)
